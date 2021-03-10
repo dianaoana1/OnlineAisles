db3639d8005f91775c2cae6489db38881440f115
sined631 <matthew.beaulieu631@gmail.com>
2021-03-10 17:54:32 -0500

    hello microsoft support

---
+++ JavaScript/ItemDetails.js
@@ -0,0 +1,9 @@
+function openDetails(name){
+    switch(name){
+        case "banana":
+            window.open("..\html\articles\bananaDescription.html","Ratting","width=550,height=170,left=150,top=200,toolbar=0,status=0,");
+            break;
+        default:
+            break;
+    }
+}
