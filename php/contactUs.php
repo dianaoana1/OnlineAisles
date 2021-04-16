0881fd5d9fdcad94c01995e0ec48bbdbfb7520be
sined631 <matthew.beaulieu631@gmail.com>
2021-04-15 22:05:13 -0400

    cancer

--- php/contactUs.php
+++ php/contactUs.php
@@ -12,24 +12,27 @@
 <body>
 
     <?php
-
-    $usernameErr = $itemNameErr = $OrderNameErr = $commentErr = "";
     $username = $itemname  = $comment = $order = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["username"])) {
-            $nameErr = "Username is required";
-        } else {
+      
+        if (!empty($_POST["username"])) {
             $username = test_input($_POST["username"]);
-        }
+    }
 
-        if (empty($_POST["itemname"])) {
+        if (empty($_POST["itemName"])) {
             $itemNameErr = "Item Name is required";
+           
+        }else if (checkitemName($_POST["itemName"])){
+          $itemNameErr="Please enter a valid itemName";
+          echo "empty mofo";
         } else {
-            $itemname = test_input($_POST["itemname"]);
+            $itemname = test_input($_POST["itemName"]);
         }
-        if (empty($_POST["order"])) {
-            $OrderNameErr = "Order Number is Empty";
+        if (empty($_POST["orderNumber"])) {
+            $orderNameErr = "Order Number is Empty";
+        } else if(checkOrderNum($_POST["orderNumber"])){
+           $orderNameErr="Order Number does Not match the #12345AB format";
         } else {
             $order = test_input($_POST["comment"]);
         }
@@ -47,25 +50,28 @@
         $data = htmlspecialchars($data);
         return $data;
     }
-    function checkOrderNum($orderNum)
+    function  checkitemName($itemname)
     {
-        $reg="/[a-zA-z]/";
-    }/*
-    function checkOrderNum() {
-    var order_num = document.getElementById("orderNum").value;
-    const regexp2=new RegExp("^#{1}[0-9]{5}[A-Za-z]{2}$");
-    console.log(regexp2.test(order_num));
-    if(order_num.length==0){
-        alert("Please enter the order number");
-    }else  if (regexp2.test(order_num)) {
-        return true;
+        $reg="/[0-9]/";
+        if(preg_match($reg,$itemname)){
+            return true;
+        }else{
+            return false;
+        }
     }
-    else {
-       alert('Order number does not follow expected format. Should be of the form #12345AB.');
-        return false;
+    function checkOrderNum($orderNum){
+       $reg="/^#{1}[0-9]{5}[A-Za-z]{2}$/";
+       if(strlen($orderNum)!=8){
+           return false;
+       }else if (preg_match($reg,$orderNum)) {
+            return true;
+       }else{
+           return false;
+       }
     }
-   
-}*/
+    
+    
+    
     ?>
 
 
