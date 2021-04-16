fdd381cb9a8aca60ffa5f66befd9aa63baa7242f
sined631 <matthew.beaulieu631@gmail.com>
2021-04-16 14:30:29 -0400

    Yessir

--- php/contactUs.php
+++ php/contactUs.php
@@ -14,30 +14,40 @@
     <?php
     $username = $itemname  = $comment = $order = "";
     $user_exists = false;
+    $file=fopen("..\TextFiles\logsContactus.txt","a+");
+    fwrite($file2,"a");
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        fwrite($file, "Start of this comment:\n");
         //username part
         if (!empty($_POST["username"])) {
             $username = test_input($_POST["username"]);
+            fwrite($file,"User:".$username. "\n");
             echo $username . "</br>";
+
         }
         //itemname part
         if (!empty($_POST["itemName"]) && !checkitemName($_POST["itemName"])) {
             $itemname = test_input($_POST["itemName"]);
+            fwrite($file,"Item Name:".$itemname. "\n");
             echo $itemname . "</br>";
         }
 
         //ordernumber part
         if (!empty($_POST["orderNumber"]) && checkOrderNum($_POST["orderNumber"])) {
             $order = test_input($_POST["orderNumber"]);
+            fwrite($file,"Order Number:".$order. "\n\n");
             echo $order . "</br>";
         }
         //comment part
         if (!empty($_POST["comment"])) {
             $comment = test_input($_POST["comment"]);
+            fwrite($file,$comment. "\n");
             echo $comment . "</br>";
         }
-        if (!isset($_SESSION) || !usernameExists($username)) {
-            echo "<script>alert('The username does not exist/or is not signed on currently');document.location='../html/Signup.html'</script>";
+        if (!isset($_SESSION)) {
+            echo "<script>alert('This user is not signed on currently');document.location='../html/Login.html'</script>";
+         } else if (!usernameExists($username)) {
+            echo "<script>alert('The username does not exist');document.location='../html/Signup.html'</script>";
         } else {
             echo "<script>alert('Thank you for the comment we will make sure to do better have a great day ');document.location='../html/shopping_cart.html'</script>";
         }
