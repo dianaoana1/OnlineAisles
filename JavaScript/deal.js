6c7fc954e12c336a81e07a577fa5140a9e669fef
sined631 <matthew.beaulieu631@gmail.com>
2021-04-16 20:36:46 -0400

    deals

--- JavaScript/deal.js
+++ JavaScript/deal.js
@@ -7,6 +7,15 @@ function dealChecker() {
     var currPriceApple = "<?php echo $bananaprice ?>";
     var currPriceBread = "<?php echo $bananaprice ?>";
     var currPriceBroccoli = "<?php echo $bananaprice ?>";
+    if (currPriceBannana == bananaFirstPrice && appleFirstPrice == currPriceApple && currPriceBread == breadFirstPrice && broccoliFirstPrice == currPriceBroccoli) {
+        document.getElementById("promo").innerHTML = "No deals this Week";
+    } else if (currPriceBannana == bananaFirstPrice && appleFirstPrice == currPriceApple) {
+        var idrow = document.getElementById("row1");
+        idrow.parentNode.removeChild(idrow);
+    } else if (currPriceBread == breadFirstPrice && broccoliFirstPrice == currPriceBroccoli) {
+        var idrow = document.getElementById("row2");
+        idrow.parentNode.removeChild(idrow);
+    }else{
     if (currPriceBannana == bananaFirstPrice) {
         bananaChanger();
     }
@@ -19,28 +28,26 @@ function dealChecker() {
     if (broccoliFirstPrice == currPriceBroccoli) {
         broccoliChanger();
     }
-    if(currPriceBannana == bananaFirstPrice&&appleFirstPrice == currPriceApple&&currPriceBread == breadFirstPrice&&broccoliFirstPrice == currPriceBroccoli){
-        document.getElementById("promo").innerHTML="No deals this Week";
     }
 }
 
 function bananaChanger() {
-    var idBanana=  document.getElementById("col-banana");
+    var idBanana = document.getElementById("col-banana");
     idBanana.parentNode.removeChild(idBanana);
-    document.getElementById("col-banana").style.position="relative";
+    document.getElementById("col-apple").style.position = "relative";
 }
 function appleChanger() {
-    var idApple=  document.getElementById("col-apple");
+    var idApple = document.getElementById("col-apple");
     idApple.parentNode.removeChild(idApple);
-    document.getElementById("col-banana").style.position="relative";
+    document.getElementById("col-banana").style.position = "relative";
 }
 function breadChanger() {
-    var idBread=  document.getElementById("col-bread");
+    var idBread = document.getElementById("col-bread");
     idBread.parentNode.removeChild(idBread);
-    document.getElementById("col-banana").style.position="relative";
+    document.getElementById("col-broccoli").style.position = "relative";
 }
 function broccoliChanger() {
-    var idBrocc=  document.getElementById("col-broccoli");
+    var idBrocc = document.getElementById("col-broccoli");
     idBrocc.parentNode.removeChild(idBrocc);
-    document.getElementById("col-banana").style.position="relative";
+    document.getElementById("col-bread").style.position = "relative";
 }
