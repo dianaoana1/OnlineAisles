6e41afa166bb9b69381422637c761fbb42fe26d5
Alexandra <aspyridakos@gmail.com>
2021-04-18 13:55:05 -0400

    added password to userlist

--- php/userListFunctions.php
+++ php/userListFunctions.php
@@ -22,6 +22,9 @@ function addUser()
                 <label for="username"><b>Username</b></label>
                 <input type="text" pattern="[a-zA-Z0-9]{1,20}" placeholder="Enter username" id="usernameForm" name="username" required />
 
+                <label for="password"><b>Password</b></label>
+                <input required type="text" placeholder="Enter password" id="passwordForm" name="password"/> 
+
                 <label for="lastName"><b>Last name</b></label>
                 <input type="text" pattern="[a-zA-Z]{1,20}" placeholder="Enter last name" id="lastNameForm" name="lastName" required />
 
@@ -47,13 +50,14 @@ function addUser()
     }
     if (isset($_POST['add'])) {
         $username = $_POST['username'];
+        $password = $_POST['password'];
         $lastName = $_POST['lastName'];
         $firstName = $_POST['firstName'];
         $email = $_POST['email'];
         $tel1 = $_POST['tel1'];
         $tel2 = $_POST['tel2'];
         $address = $_POST['address'];
-        addUserInfoToFile($username, $lastName, $firstName, $email, $tel1, $tel2, $address);
+        addUserInfoToFile($username, $password, $lastName, $firstName, $email, $tel1, $tel2, $address);
         header('Location: userList.php');
     }
 }
@@ -115,11 +119,6 @@ $file=fopen("E:\MAMP\htdocs/287assignment1\TextFiles\initialUserDatabase.txt","r
  fclose($file);
 */
 
-//check email input
-/*$email = test_input($_POST["email"]);
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  $emailErr = "Invalid email format";
-}*/
 function editUser($userID)
 {
     $thisPage = htmlspecialchars($_SERVER["PHP_SELF"]);
@@ -131,12 +130,13 @@ function editUser($userID)
             if ($lineCount == $userID) {
                 $lineArr = explode("\t", $line);
                 $username = $lineArr[0];
-                $lastName = $lineArr[1];
-                $firstName = $lineArr[2];
-                $email = $lineArr[3];
-                $tel1 = $lineArr[4];
-                $tel2 = $lineArr[5];
-                $address = $lineArr[6];
+                $password = $lineArr[1];
+                $lastName = $lineArr[2];
+                $firstName = $lineArr[3];
+                $email = $lineArr[4];
+                $tel1 = $lineArr[5];
+                $tel2 = $lineArr[6];
+                $address = $lineArr[7];
             }
             $lineCount++;
         }
@@ -146,6 +146,9 @@ function editUser($userID)
                 <h3>Editing User</h3>
                 <label for="username"><b>Username</b></label>
                 <input required type="text" pattern="[a-zA-Z0-9]{1,20}" placeholder="Enter username" id="usernameForm" name="username" value="<?php echo $username; ?>" />
+
+                <label for="password"><b>Password</b></label>
+                <input required type="text" placeholder="Enter password" id="passwordForm" name="password" value="<?php echo $password; ?>" />  
 
                 <label for="lastName"><b>Last name</b></label>
                 <input required type="text" pattern="[a-zA-Z]{1,20}" placeholder="Enter last name" id="lastNameForm" name="lastName" value="<?php echo $lastName; ?>" />
@@ -174,26 +177,28 @@ function editUser($userID)
     }
     if (isset($_POST['edit'])) {
         $username = $_POST['username'];
+        $password = $_POST['password'];
         $lastName = $_POST['lastName'];
         $firstName = $_POST['firstName'];
         $email = $_POST['email'];
         $tel1 = $_POST['tel1'];
         $tel2 = $_POST['tel2'];
         $address = $_POST['address'];
-        editUserInfoFile($username, $lastName, $firstName, $email, $tel1, $tel2, $address);
+        editUserInfoFile($username, $password, $lastName, $firstName, $email, $tel1, $tel2, $address);
         header('Location: userList.php');
     }
 }
 
 
 //adds row to the table being generated
-function newRow($count, $username, $lastName, $firstName, $email, $tel1, $tel2, $address)
+function newRow($count, $username, $password, $lastName, $firstName, $email, $tel1, $tel2, $address)
 {
     $userID = $count;
     if ($count % 2 == 0) {
         return "<tr>
         <td class=\"tg-even\">" . $userID . "</td>
         <td class=\"tg-even\">" . $username . "</td>
+        <td class=\"tg-even\">" . $password . "</td>
         <td class=\"tg-even\">" . $lastName . "</td>
         <td class=\"tg-even\">" . $firstName . "</td>
         <td class=\"tg-even\"><a href=" . $tel1 . ">" . $tel1 . "</a></td>
@@ -205,6 +210,7 @@ function newRow($count, $username, $lastName, $firstName, $email, $tel1, $tel2, 
         return "<tr>
             <td class=\"tg-odd\">" . $userID . "</td>
             <td class=\"tg-odd\">" . $username . "</td>
+            <td class=\"tg-odd\">" . $password . "</td>
             <td class=\"tg-odd\">" . $lastName . "</td>
             <td class=\"tg-odd\">" . $firstName . "</td>
             <td class=\"tg-odd\"><a href=" . $tel1 . ">" . $tel1 . "</a></td>
@@ -223,6 +229,7 @@ function ProcessEmptyTable()
                     <tr>
                         <th class="tg-header">User ID</th>
                         <th class="tg-header">Username</th>
+                        <th class="tg-header">Password</th>
                         <th class="tg-header">Last name</th>
                         <th class="tg-header">First Name</th>
                         <th class="tg-header">Tel.1</th>
@@ -233,7 +240,7 @@ function ProcessEmptyTable()
                 </thead>
                 <tbody>
                     <tr>
-                        <td class="tg-even" colspan="8">No users have been registered yet aside from the admin role</td>
+                        <td class="tg-even" colspan="9">No users have been registered yet aside from the admin role</td>
                     </tr>
                 </tbody>
                 </table></div><div style="position:center; text-align:center; margin:15px;font-family:Arial, sans-serif;font-size:20px;font-weight:normal">
@@ -250,6 +257,7 @@ function ProcessUsersToTable()
     <thead>
     <th class=\"tg-header\">User ID</th>
     <th class=\"tg-header\">Username</th>
+    <th class=\"tg-header\">Password</th>
     <th class=\"tg-header\">Last name</th>
     <th class=\"tg-header\">First Name</th>
     <th class=\"tg-header\">Tel.1</th>
@@ -263,7 +271,7 @@ function ProcessUsersToTable()
         $count++;
         $line = fgets($file);
         $lineArr = explode("\t", $line);
-        $result .= newRow($count, $lineArr[0], $lineArr[1], $lineArr[2], $lineArr[3], $lineArr[4], $lineArr[5], $lineArr[6]);    //adding new table rows
+        $result .= newRow($count, $lineArr[0], $lineArr[1], $lineArr[2], $lineArr[3], $lineArr[4], $lineArr[5], $lineArr[6], $lineArr[7]);    //adding new table rows
     }
     $result .= "</tbody></table></div>
     <div style=\"position:center; text-align:center; margin:15px;font-family:Arial, sans-serif;font-size:20px;font-weight:normal\">
@@ -283,6 +291,7 @@ function ProcessUserList()
     <thead>
     <th class=\"tg-header\">User ID</th>
     <th class=\"tg-header\">Username</th>
+    <th class=\"tg-header\">Password</th>
     <th class=\"tg-header\">Last name</th>
     <th class=\"tg-header\">First Name</th>
     <th class=\"tg-header\">Tel.1</th>
@@ -296,19 +305,19 @@ function ProcessUserList()
         $count++;
         $line = fgets($file);
         $lineArr = explode("\t", $line);
-        $result .= newRow($count, $lineArr[0], $lineArr[1], $lineArr[2], $lineArr[3], $lineArr[4], $lineArr[5], $lineArr[6]);    //adding new table rows
+        $result .= newRow($count, $lineArr[0], $lineArr[1], $lineArr[2], $lineArr[3], $lineArr[4], $lineArr[5], $lineArr[6], $lineArr[7]);    //adding new table rows
     }
     $result .= "</tbody></table>";
     fclose($file);
     echo $result;
 }
 
-function addUserInfoToFile($username, $lastName, $firstName, $email, $tel1, $tel2, $address)
+function addUserInfoToFile($username, $password, $lastName, $firstName, $email, $tel1, $tel2, $address)
 {
     if (fileIsEmpty($_SESSION['file'])) {
-        $userInfo = $username . "\t" . $lastName . "\t" . $firstName . "\t" . $email . "\t" . $tel1 . "\t" . $tel2 . "\t" . $address;
+        $userInfo = $username ."\t" . $password . "\t" . $lastName . "\t" . $firstName . "\t" . $email . "\t" . $tel1 . "\t" . $tel2 . "\t" . $address;
     } else {
-        $userInfo =  "\n" . $username . "\t" . $lastName . "\t" . $firstName . "\t" . $email . "\t" . $tel1 . "\t" . $tel2 . "\t" . $address;
+        $userInfo =  "\n" . $username . "\t" . $password ."\t" . $lastName . "\t" . $firstName . "\t" . $email . "\t" . $tel1 . "\t" . $tel2 . "\t" . $address;
     }
     if (file_exists($_SESSION['file'])) {
         $file = fopen($_SESSION['file'], 'a') or die("Unable to open 'userInfo.txt'.");
@@ -319,9 +328,9 @@ function addUserInfoToFile($username, $lastName, $firstName, $email, $tel1, $tel
     }
 }
 
-function editUserInfoFile($username, $lastName, $firstName, $email, $tel1, $tel2, $address)
+function editUserInfoFile($username, $password, $lastName, $firstName, $email, $tel1, $tel2, $address)
 {
-    $userInfo =  $username . "\t" . $lastName . "\t" . $firstName . "\t" . $email . "\t" . $tel1 . "\t" . $tel2 . "\t" . $address;
+    $userInfo =  $username . "\t" . $password ."\t" . $lastName . "\t" . $firstName . "\t" . $email . "\t" . $tel1 . "\t" . $tel2 . "\t" . $address;
     $file = fopen("..\TextFiles\userInfo.txt", 'r') or die("Unable to open 'userInfo.txt'.");
     $tempFile = fopen("..\TextFiles\\tempUserInfo.txt", 'w') or die("Unable to open 'tempUserInfo.txt'.");
     $lineCount = 1;
