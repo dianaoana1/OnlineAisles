c28cc66d581385ecc8e907ad1dfc721963182850
sined631 <matthew.beaulieu631@gmail.com>
2021-04-19 15:41:02 -0400

    final links items

--- php/articles/sausages.php
+++ php/articles/sausages.php
@@ -36,8 +36,8 @@ $productCurrPrice=getPrice($product);
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-md bg-dark navbar-dark ">
-        <a class="navbar-brand" href="..\Main Page.html">Online Aisles</a>
+<nav class="navbar navbar-expand-md bg-dark navbar-dark ">
+        <a class="navbar-brand" href="..\Main Page.php">Online Aisles</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -49,19 +49,20 @@ $productCurrPrice=getPrice($product);
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Aisles</a>
 
                         <div class="dropdown-menu" aria-labelledby="dropdown03">
-                            <a class="dropdown-item" href="../Product links/fruits.html"
-                                target="_self">Fruits</a>
-                                <a class="dropdown-item" href="../Product links/Vegetables.html"
-                                target="_self">Vegetables</a>
-                            <a class="dropdown-item" href="../Product links/meats and poultry.html" target="_self">
+                            <a class="dropdown-item" href="../Product links/fruits.php"
+                                target="_self">Fruits
+                                </a>
+                            <a class="dropdown-item" href="../Product links/Vegetables.php" target="_self">Vegetables </a>
+                            <a class="dropdown-item" href="../Product links/meats and poultry.php" target="_self">
                                 Meats
                                 and Poultry </a>
-                            <a class="dropdown-item" href="../Product links/seafood.html" target="_self">Seafood </a>
-                            <a class="dropdown-item" href="../Product links/dairy and eggs.html" target="_self">Dairy
+                            <a class="dropdown-item" href="../Product links/seafood.php" target="_self">Seafood </a>
+                            <a class="dropdown-item" href="../Product links/dairy and eggs.php" target="_self">Dairy
                                 and
                                 Eggs </a>
-                            <a class="dropdown-item" href="../Product links/cereal products.html" target="_self">Cereal
+                            <a class="dropdown-item" href="../Product links/cereal products.php" target="_self">Cereal
                                 Products </a>
+                            
                         </div>
                     </div>
                 </li>
@@ -73,7 +74,7 @@ $productCurrPrice=getPrice($product);
                         <div class="dropdown-menu" aria-labelledby="dropdown03">
                             <a class="dropdown-item" href="..\..\php\userList.php" target="_self">User List</a>
                             <a class="dropdown-item" href="..\..\php\productList.php" target="_self"> Product list </a>
-                            <a class="dropdown-item" href="..\back end\Order-list.html" _self">Order List </a>
+                            <a class="dropdown-item" href="..\..\php\orderList.php" target="_self">Order List </a>
                         </div>
                 </li>
                 <li class="nav-item">
@@ -85,11 +86,11 @@ $productCurrPrice=getPrice($product);
                         <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">&#9881;</a>
                         <div class="dropdown-menu" aria-labelledby="dropdown03">
-                            <a class="dropdown-item" href="..\Back end\change password.html" target="_blank"> Change
+                        <a class="dropdown-item" href="..\..\html\Back end\change password.html" target="_blank"> Change
                                 Password</a>
-                            <a class="dropdown-item" href="..\Back end\account-info.html" target="_self"> Account
-                                Info</a></a>
-                            <a class="dropdown-item" href="..\Back end\delete-user.html" target="_blank">Delete
+                            <a class="dropdown-item" href="..\..\html\Back end\account-info.html" target="_self"> Account
+                                Info</a>
+                            <a class="dropdown-item" href="..\..\html\Back end\delete-user.php" target="_blank">Delete
                                 Account</a>
                         </div>
                     </div>
