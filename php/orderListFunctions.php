c6b50d0e85134c8561758f745e4ae3440b6ba8bf
Zeebruh326 <80176518+zeeewaaa@users.noreply.github.com>
2021-04-19 20:33:17 -0400

    -

--- php/orderListFunctions.php
+++ php/orderListFunctions.php
@@ -16,8 +16,6 @@ function addOrder()
                 <label for="quantity"><b>Quantity</b></label>
                 <input type="text" pattern="[0-9]{1,3}" placeholder="Enter item quantity" id="quantityForm" name="quantity" required />
 
-                <label for="address"><b>Address</b></label>
-                <input type="text" placeholder="Enter address" id="addressForm" name="address" required />
                 <br>
                 <button type="submit" class="btn btn-primary" name="add">Add New Order</button>
             </form>
@@ -57,12 +55,11 @@ function ProcessEmptyOrderTable(){
             <th class="tg-header">User</th>
             <th class="tg-header">Item Number</th>
             <th class="tg-header">Quantity</th>
-            <th class="tg-header">Address</th>
         </tr>
         </thead>
         <tbody>
             <tr>
-                <td class="tg-even" colspan="5">No orders yet</td>
+                <td class="tg-even" colspan="4">No orders yet</td>
             </tr>
         </tbody>
         </table></div><div style="position:center; text-align:center; margin:15px;font-family:Arial, sans-serif;font-size:20px;font-weight:normal">
@@ -80,23 +77,213 @@ function ProcessOrdersToTable(){
     <th class="tg-header">User</th>
     <th class="tg-header">Item Number</th>
     <th class="tg-header">Quantity</th>
-    <th class="tg-header">Address</th>
     <tbody>";
     $count = 0;
     while (!feof($file)) {   //reading file line by line
         $count++;
         $line = fgets($file);
         $lineArr = explode("\t", $line);
-        $result .= newRow($count, $lineArr[0], $lineArr[1], $lineArr[2], $lineArr[3]);    //adding new table rows
+        $result .= newRow($count, $lineArr[0], $lineArr[1], $lineArr[2]);    //adding new table rows
     }
     $result .= "</tbody></table></div>
     <div style=\"position:center; text-align:center; margin:15px;font-family:Arial, sans-serif;font-size:20px;font-weight:normal\">
             <a type=\"text\" name=\"add-order-button\" style=\"font-weight: bold;\" class=\"btn btn-primary\" href=\"addOrder.php\">Add New Order</a>
-            <a type=\"text\" name=\"edit-order-button\" style=\"font-weight: bold;\" class=\"btn btn-primary\" href=\"editOrder.php\">Edit User</a>
-            <a type=\"text\" name=\"delete-order-button\" style=\"font-weight: bold;\" class=\"btn btn-primary\" href=\"deleteOrder.php\">Delete User</a>
+            <a type=\"text\" name=\"edit-order-button\" style=\"font-weight: bold;\" class=\"btn btn-primary\" href=\"editOrder.php\">Edit Order</a>
+            <a type=\"text\" name=\"delete-order-button\" style=\"font-weight: bold;\" class=\"btn btn-primary\" href=\"deleteOrder.php\">Delete Order</a>
         </div>";
     fclose($file);
     echo $result;
 
 }
+
+function newRow($count, $ordernum, $username, $itemNum, $quantity)
+{
+    $ordernum = $count;
+    if ($count % 2 == 0) {
+        return "<tr>
+        <td class=\"tg-even\">" . $ordernum . "</td>
+        <td class=\"tg-even\">" . $username . "</td>
+        <td class=\"tg-even\">" . $itemNum . "</td>
+        <td class=\"tg-even\">" . $quantity . "</td>
+    </tr>";
+    } else {
+        return "<tr>
+        <td class=\"tg-even\">" . $ordernum . "</td>
+        <td class=\"tg-even\">" . $username . "</td>
+        <td class=\"tg-even\">" . $itemNum . "</td>
+        <td class=\"tg-even\">" . $quantity . "</td>
+        
+        </tr>";
+    }
+}
+
+function ProcessOrderList()
+{
+    $file = fopen($_SESSION['file'], "a+");
+    $result = "";
+    $result .= "<table class=\"tg\" id=\"orderTable\" name = \"orderTable\">
+    <thead>
+    <th class="tg-header">Order Number</th>
+    <th class="tg-header">User</th>
+    <th class="tg-header">Item Number</th>
+    <th class="tg-header">Quantity</th>
+    <tbody>";
+    $count = 0;
+    while (!feof($file)) {   //reading file line by line
+        $count++;
+        $line = fgets($file);
+        $lineArr = explode("\t", $line);
+        $result .= newRow($count, $lineArr[0], $lineArr[1], $lineArr[2]);
+    }
+    $result .= "</tbody></table>";
+    fclose($file);
+    echo $result;
+}
+
+function addOrderInfoToFile($ordernum, $username, $itemNum, $quantity)
+{
+    if (fileIsEmpty($_SESSION['file'])) {
+        $orderInfo = $ordernum ."\t" . $username ."\t" . $itemNum . "\t" . $quantity;
+    } else {
+        $orderInfo =  "\n" . $ordernum ."\t" . $username ."\t" . $itemNum . "\t" . $quantity;
+    }
+    if (file_exists($_SESSION['file'])) {
+        $file = fopen($_SESSION['file'], 'a') or die("Unable to open 'orderInfo.txt'.");
+        fwrite($file, $orderInfo);
+        fclose($file);
+    } else {
+        echo "Unable to open 'orderInfo.txt'.";
+    }
+}
+
+function getLastLine(){
+    $file = fopen("..\TextFiles\orderInfo.txt", 'r') or die("Unable to open 'orderInfo.txt'.");
+    $lineCount = 0;
+    while (!feof($file)) {
+        fgets($file);
+        $lineCount++;
+    }
+    return $lineCount;
+    fclose($file);
+}
+
+function editOrderInfoFile($ordernum, $username, $itemNum, $quantity)
+{
+    $orderInfo = $username ."\t" . $itemNum . "\t" . $quantity;
+    $file = fopen("..\TextFiles\orderInfo.txt", 'r') or die("Unable to open 'orderInfo.txt'.");
+    $tempFile = fopen("..\TextFiles\\tempOrderInfo.txt", 'w') or die("Unable to open 'tempOrderInfo.txt'.");
+    $lineCount = 1;
+    //copying userInfo.txt to tempOrderInfo.txt
+    $lastLine = getLastLine();
+    echo $lastLine;
+    while (!feof($file)) {
+        $line = fgets($file);
+        if ($lineCount==$ordernum) {
+            if ($lineCount == $lastLine){
+                echo $lastLine;
+                fwrite($tempFile, $orderInfo);
+            }
+            else{
+                fwrite($tempFile, $orderInfo."\n");
+            }
+        }
+        else{
+            fwrite($tempFile, $line);
+        }
+        $lineCount++;
+    }
+    fclose($tempFile);
+    fclose($file);
+    //clearing 
+    $file = fopen("..\TextFiles\orderInfo.txt", 'w') or die("Unable to open 'orderInfo.txt'.");
+    fclose($file);
+    $file = fopen("..\TextFiles\orderInfo.txt", 'a') or die("Unable to open 'orderInfo.txt'.");
+    $tempFile = fopen("..\TextFiles\\tempOrderInfo.txt", 'r') or die("Unable to open 'tempOrderInfo.txt'.");
+    //putting back all order info into orderInfo.txt
+    while (!feof($tempFile)) {
+        $line = fgets($tempFile);
+        fwrite($file, $line);
+    }
+    fclose($file);
+    fclose($tempFile);
+}
+
+function deleteOrderFromFile($ordernum)
+{
+    $file = fopen("..\TextFiles\orderInfo.txt", 'r') or die("Unable to open 'orderInfo.txt'.");
+    $tempFile = fopen("..\TextFiles\\tempOrderInfo.txt", 'w') or die("Unable to open 'tempOrderInfo.txt'.");
+    $lineCount = 1;
+    //copying orderInfo.txt to tempOrderInfo.txt
+    while (!feof($file)) {
+        $line = fgets($file);
+        if ($lineCount!=$ordernum) {
+            fwrite($tempFile, $line);
+        }
+        $lineCount++;
+    }
+    fclose($tempFile);
+    fclose($file);
+    //clearing orderInfo.txt
+    $file = fopen("..\TextFiles\orderInfo.txt", 'w') or die("Unable to open 'orderInfo.txt'.");
+    fclose($file);
+    $file = fopen("..\TextFiles\orderInfo.txt", 'a') or die("Unable to open 'orderInfo.txt'.");
+    $tempFile = fopen("..\TextFiles\\tempOrderInfo.txt", 'r') or die("Unable to open 'tempOrderInfo.txt'.");
+    //putting back all order info into orderInfo.txt except the deleted order
+    while (!feof($tempFile)) {
+        $line = fgets($tempFile);
+        fwrite($file, $line);
+    }
+    fclose($file);
+    fclose($tempFile);
+}
+
+function editUser($ordernum)
+{
+    $thisPage = htmlspecialchars($_SERVER["PHP_SELF"]);
+    if (!isset($_POST['edit'])) {
+        $file = fopen("..\TextFiles\orderInfo.txt", 'r') or die("Unable to open 'orderInfo.txt'.");
+        $lineCount = 1;
+        while (!feof($file)) {   //reading file line by line
+            $line = fgets($file);
+            if ($lineCount == $ordernum) {
+                $lineArr = explode("\t", $line);
+                $ordernum = $lineArr[0];
+                $username = $lineArr[1];
+                $itemNum = $lineArr[2];
+                $quantity = $lineArr[3];
+                
+            }
+            $lineCount++;
+        }
+?>
+        <div class="form-popup" id="editOrderForm">
+            <form action="<?php echo $thisPage; ?>" class="form-container" method="POST">
+                <h3>Editing Order</h3>
+                <label for="username"><b>username</b></label>
+                <input required type="text" pattern="[a-zA-Z0-9]{1,20}" placeholder="Enter username" id="usernameForm" name="username" value="<?php echo $username; ?>" />
+
+                <label for="itemNum"><b>item number</b></label>
+                <input required type="text" placeholder="Enter item number" id="Enter itemNum" name="itemNumForm" value="<?php echo $itemNum; ?>" />  
+
+                <label for="lastName"><b>quantity</b></label>
+                <input required type="text" pattern="[a-zA-Z]{1,20}" placeholder="Enter quantity" id="quantityForm" name="quantity" value="<?php echo $quantity; ?>" />
+
+                <br>
+                <button type="submit" class="btn btn-primary" name="edit">Save Changes</button>
+            </form>
+        </div>
+<?php
+    } else {
+        echo "Error occured.<br>Unable to open 'orderInfo.txt'.";
+    }
+    if (isset($_POST['edit'])) {
+        $username = $_POST['username'];
+        $itemNum = $_POST['itemNum'];
+        $quantity = $_POST['quantity'];
+        
+        editUserInfoFile($orderNum, $username, $itemNum, $quantity);
+        header('Location: orderList.php');
+    }
+}
+
 ?>
